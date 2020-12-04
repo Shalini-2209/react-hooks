2f352b2f9085b1ace3af9447a3b5dc3e19c461f8
Ahana <ahana298@gmail.com>
2020-12-04 20:43:15 +0530

    useContext hooks

--- src/Components/ComponentB.jsx
+++ src/Components/ComponentB.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { UserContext, WishContext } from "./ComponentA";
-
+// useContext usage
 const ComponentB = () => {
   const user = useContext(UserContext);
   const wish = useContext(WishContext);
